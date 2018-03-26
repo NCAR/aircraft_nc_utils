4887e5bcbe6dab6f50c12f77a84a0efda33cf313
Janine Aquino <janine@ucar.edu>
2018-03-26 15:46:16 -0600

    Update link to NetCDF error codes given in error message

--- asc2cdf/error.cpp
+++ asc2cdf/error.cpp
@@ -18,7 +18,7 @@ void handle_error(int status)
 {
     fprintf(stderr, "\n*****\n");
     fprintf(stderr, "Caught error %d\n",status);
-    fprintf(stderr, "See http://www.unidata.ucar.edu/software/netcdf/docs/netcdf-c/NetCDF_002d3-Error-Codes.html for explanation\n");
+    fprintf(stderr, "See https://www.unidata.ucar.edu/software/netcdf/netcdf-4/newdocs/netcdf-c/NetCDF_002d3-Error-Codes.html for explanation\n");
     fprintf(stderr, "*****\n");
     exit(1);
 }
